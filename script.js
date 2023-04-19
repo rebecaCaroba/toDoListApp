5536c3f9af57a5dd10ce85a88843d5279dc6873e
rebecaCaroba <rebecamartin200@gmail.com>
2023-04-19 18:22:09 -0300

    Initial Commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const form = document.querySelector('form');
+const res = document.querySelector('#res');
+
+const arrTask = [];
+const cont = 0;
+
+function criarItem(valor){
+    const itemElementoDiv = document.createElement('div');
+    itemElementoDiv.classList.add("tasks-res")
+
+    const spanValor = document.createElement("span")
+    spanValor.innerText = valor
+    itemElementoDiv.appendChild(spanValor)
+
+    
+}
+
+
+form.addEventListener('submit', function addElemento(e){
+    e.preventDefault();
+
+    const valor = document.querySelector('#text');
+    if(valor) {
+        const itemList = criarItem(valor.value);
+    }
+})
