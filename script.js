d32e87df631a527bcc2e45a965d84fa3b52fa433
rebecaCaroba <rebecacaroba@gmail.com>
2023-05-06 17:17:42 -0300

    Funcionalidade de update feita e melhoria no codigo

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const add = document.querySelector('.tasks-input');
-const spanTask = document.querySelector('.tasks-span');
 const res = document.querySelector('#res');
 const noTask = document.querySelector('.no-task');
+const cancel = document.querySelector('#cancel');
 const btnAll = document.querySelector('#All');
 const btnActive = document.querySelector('#Active');
 const btnCompleted = document.querySelector('#Completed');
@@ -9,7 +9,7 @@ const btnClear = document.querySelector('#Clear');
 const amout = document.querySelector('#amout');
 const allDatas = JSON.parse(localStorage.getItem('datas'));
 let itens = localStorage.getItem('datas') !== null ? allDatas : [];
-const charMax = 30;
+const charMax = 60;
 
 
 function IdAleatorio() {
@@ -35,7 +35,7 @@ add.addEventListener("submit", (event) => {
         Renderizar();
         atualizarLocalStorage();
         inputValor.value = ' '
-        inputValor.focus()
+        inputValor.focus();
     }
 });
 
@@ -44,17 +44,25 @@ function atualizarLocalStorage() {
 }
 
 function Renderizar() {
-    res.innerHTML = ' '
-    itens.forEach(item => {
+    res.innerHTML = ' ';
+    itens.map(item => {
         res.insertAdjacentHTML("afterbegin", `
                 <div class="tasks-res" id="${item.id}">
                     <form class="tasks">
-                        <input type="checkbox" class="checkbox-round">
-                        <span class="tasks-span" contenteditable="true">
+                        <input type="checkbox" class="checkbox-round" id='exemplo'>
+                        <button type="submit" class="button-edit hidden" onClick="Update(${item.id})" ><i class="fa-solid fa-right-left" style="color: #494c6b;"></i></button>
+                        <span class="tasks-span">
                             ${item.valor}
                         </span>
+                        <span class="input-span">
+                        <input type="text" id="editText-${item.id}" class="hidden editText">
+                        </span>
                     </form>
+                    <div class="tasks-btn">
+                    <button class="button-toggle" onClick='toggleForm(${item.id})'><i class="fa-solid fa-pen" style="color: #494c6b;"></i></button>
                     <button class="button-cross" onClick='Delete(${item.id})' />
+                    <button class="cancel hidden" onClick=Cancelar(${item.id})><i class="fa-solid fa-circle-xmark" style="color: #494c6b;"></i></button>
+                    </div>
                 </div>`
         )
     });
@@ -67,11 +75,11 @@ function contagemDeItens() {
 function Delete(id) {
     itens = itens.filter(item => item.id !== id);
     const itemremove = document.getElementById(`${id}`);
+    atualizarLocalStorage();
     itemremove.remove();
-
     contagemDeItens();
     renderizarListaVazia();
-    atualizarLocalStorage();
+
 }
 
 function renderizarListaVazia() {
@@ -87,17 +95,51 @@ btnClear.addEventListener('click', () => {
     renderizarListaVazia();
 });
 
+function toggleForm(id) {
+    const taskRes = document.querySelector(`.tasks-res[id="${id}"]`);
+    const tasks = document.querySelector('.tasks');
+    const btnEdit = taskRes.querySelector('.button-edit');
+    const checkbox = taskRes.querySelector('.checkbox-round');
+    const spanTask = taskRes.querySelector('.tasks-span');
+    const inputEdit = taskRes.querySelector(`#editText-${id}`);
+    const btnUpdate = taskRes.querySelector('.button-toggle');
+    const btnDelete = taskRes.querySelector('.button-cross');
+    const btnCancel = taskRes.querySelector('.cancel');
 
-// Update
-document.querySelector('#res').addEventListener('input', (e) => {
-    const targetEl = e.target;
-    const editEl = targetEl.innerText;
-    const idEl = Number(targetEl.parentElement.parentElement.id);
-    const objeto = itens.find(item => item.id === idEl)
-    objeto.valor = editEl;
-    atualizarLocalStorage();
+    btnEdit.classList.toggle('hidden');
+    checkbox.classList.toggle('hidden');
+    spanTask.classList.toggle('hidden');
+    inputEdit.classList.toggle('hidden');
+    btnUpdate.classList.toggle('hidden');
+    btnDelete.classList.toggle('hidden');
+    btnCancel.classList.toggle('hidden');
 
-});
+    const editEl = spanTask.innerText;
+    inputEdit.value = editEl.trim();
+
+}
+
+function Update(id) {
+    const taskRes = document.querySelector(`.tasks-res[id="${id}"]`);
+    const inputEdit = taskRes.querySelector(`#editText-${id}`);
+    const objeto = itens.find(item => item.id === id);
+    const valor = inputEdit.value.trim();
+  
+    if (valor === "" || valor.split(" ").join("").length > charMax) {
+      alert("Invalid Task");
+
+    } else {
+      objeto.valor = valor;
+      atualizarLocalStorage();
+      toggleForm(id);
+      Renderizar();
+    }
+  }
+  
+
+function Cancelar(id) {
+    toggleForm(id);
+}
 
 Renderizar();
 renderizarListaVazia();
